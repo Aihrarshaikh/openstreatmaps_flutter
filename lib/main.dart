8ec496409366fbceb73e3fc88358636c29cfed8c
aihrar shaikh <aihrarshaikh1208@gmail.com>
2023-01-25 21:08:41 +0530

    added option to show map

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+import 'package:biker_app/maps_services/getroute_Data.dart';
+import 'package:biker_app/maps_services/select_destination.dart';
 import 'package:biker_app/maps_services/select_start.dart';
+import 'package:biker_app/maps_services/show_routes.dart';
 import 'package:flutter/material.dart';
-
+double startlat = 18.5204;
+double startlng = 73.8567;
+double destilat = 19.0760;
+double destilng = 72.8777;
 void main() {
   runApp(const MyApp());
 }
@@ -50,7 +56,26 @@ class _MyHomePageState extends State<MyHomePage> {
               MaterialPageRoute(builder: (context) => select_start()),
             )
           }
-              , child: Text("select start location")),
+              , child: Text("select current location "))
+          ,
+          ElevatedButton(onPressed: ()=>{
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => select_destination()),
+            )
+          }
+              , child: Text("select destination location")),
+          ElevatedButton(onPressed: ()=>{
+            mapp(startlat, startlng, destilat, destiLng)
+          }
+              , child: Text("get route data")),
+          ElevatedButton(onPressed: ()=>{
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => PolylinePage(startlat:startlat, startlng: startlng, endlat: destilat, endlng: destilng)),
+            )
+          }
+              , child: Text("select destination location")),
         ],
       ),
     );
